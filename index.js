5a87d1cea88e6d20a2e65d34d19c2ca986c655a5
sadmanalam142 <sadmanalam142@gmail.com>
2022-05-13 20:26:59 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello from doctor uncle')
+})
+
+app.listen(port, () => {
+    console.log('Successfully listening', port);
+})
