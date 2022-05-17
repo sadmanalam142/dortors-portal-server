b6f7fbc24dbd9f55aa6d0904e8a32db7c0f29914
sadmanalam142 <sadmanalam142@gmail.com>
2022-05-17 18:24:36 +0600

    getting booking data

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db("doctors_portal").collection("services");
+        const bookingCollection = client.db("doctors_portal").collection("bookings");
 
         app.get('/service', async (req, res) => {
             const query = {};
@@ -23,6 +24,39 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result)
         })
+
+        app.get('/booking', async (req, res)=> {
+            const patient = req.query.patient;
+            const query = {patient: patient};
+            const bookings = await bookingCollection.find(query).toArray();
+            res.send(bookings)
+        })
+
+        app.get('/available', async (req, res) => {
+            const date = req.query.date;
+            const services = await serviceCollection.find().toArray();
+            const query = {date: date};
+            const bookings = await bookingCollection.find(query).toArray();
+            services.forEach(service => {
+                const serviceBookings = bookings.filter(book => book.treatment === service.name);
+                const bookedSlots = serviceBookings.map(book => book.slot);
+                const available = service.slots.filter(slot => !bookedSlots.includes(slot));
+                service.slots = available;
+            });
+            res.send(services)
+        })
+
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = {treatment: booking.treatment, date: booking.date, patient: booking.patient};
+            const exist = await bookingCollection.findOne(query);
+            if(exist){
+               return res.send({success: false, booking: exist})
+            }
+            const result = await bookingCollection.insertOne(booking);
+            res.send({success: true, result})
+        })
+
     }
     finally {
         // await client.close();
